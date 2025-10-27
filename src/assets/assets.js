04fee1938fd8b8d57963b8c62a1bab6d3e0d72d5
Ruchika Sharma <ruchikasharma030504@gmail.com>
2025-10-28 00:00:15 +0530

    bg fix

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,5 @@
 import logo from "./logo1.png";
-import background from "./background.jpeg";
+import background from "./background2.jpeg";
 import menuIcon from "./menuIcon.svg";
 import closeIcon from "./closeIcon.svg";
 import zara from "./zara.jpeg"
