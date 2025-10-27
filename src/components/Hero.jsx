62d6af307bcc2a1b6be53be4e0c9fc27e9c27d1b
Ruchika Sharma <ruchikasharma030504@gmail.com>
2025-10-28 00:04:26 +0530

    bg fix againx2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import bg from '../assets/background2.jpg';
 
 const Hero = () => {
   return (
-    <div className='mt-20 flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("src/assets/background2.jpg")] bg-no-repeat bg-cover bg-center h-screen'>
+    <div className="mt-20 flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-no-repeat bg-cover bg-center h-screen" style={{ backgroundImage: `url(${bg})` }}>
       <h1 className="text-5xl font-bold mb-4">Your Journey Starts Here</h1>
       <p className="text-lg max-w-2xl">
         Discover destinations, find affordable flights, and make every trip unforgettable with Altitudes.
